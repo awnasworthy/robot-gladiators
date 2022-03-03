be90e79e17e53065ecf69bb8af592ab677079e63
awnasworthy <allyson.nasworthy@gmail.com>
2022-03-03 10:26:25 -0500

    fight round function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,32 @@
-var playerName = window.prompt ("What is your robot's name?")
-console.log(playerName)
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
-function fight() {
-    window.alert("The fight has begun!");
-  }
+var playerName = window.prompt ("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  //Alert players of round start
+  window.alert("Welcome to Robot Gladiators!");
+
+  //Subtract the value of playerAttack from enemyHealth, use result to update enemyHealth
+  enemyHealth = enemyHealth - playerAttack;
+
+  //Log a message to console to show that it worked
+  console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " +enemyHealth + " health remaining."
+  );
+  //Subtract the value of enemyAttack from playerHealth, use result to update playerHealth
+    playerHealth = playerHealth - enemyAttack;
+
+  //Log a message to the console to show that it worked
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  )
+}
   
   
-  //fight ()
+fight ();
